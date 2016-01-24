d7fb96495d8dbf9bd2a93bd902a8f663c0bee09d
sjorgens <scottjorgens@netscape.net>
2016-01-23 20:50:48 -0600

    adds endpoint router.get('/pullUserAddresses') to api and global var's in client.js for testing

--- client/client.js
+++ client/client.js
@@ -1,5 +1,9 @@
 var app = angular.module('routeApp', ['ngRoute']);
 
+//Global variables for testing purposes
+var USERID = 1;
+var ARRAYPOSITION = 1;
+
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){   //$locationProvider needed to remove #'s in HTML to access controller links
     $routeProvider
         .when('/addressdisplay', {
@@ -15,17 +19,33 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 
 app.controller('AddressDisplay', ['$scope', '$http', function($scope, $http){
     $scope.allUsers = [];
+    $scope.userAddresses = [];
 
     $scope.getUsers = function(){
         //console.log('getUsers function started...');
         $http.get('/api/pullAllUsers').success(function(response){
-            console.log(response);
+            //console.log(response);
             $scope.allUsers = response;
+
+            console.log('Username: ', $scope.allUsers[ARRAYPOSITION].name);
+            USERID = $scope.allUsers[ARRAYPOSITION].id;
+
+            getUserAddresses();
         });
     };
 
+    function getUserAddresses() {
+        console.log('getUserAddresses function started...');
+
+        $http.get('/api/pullUserAddresses/' + USERID).success(function(response){
+            $scope.userAddresses = response;
+
+            console.log('Username Address: ', $scope.userAddresses);
+        });
+    }
 }]);
 
 app.controller('OrderLookup', ['$scope', function($scope){
 
 }]);
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -10,8 +10,6 @@ var router = express.Router();
 
 router.get('/pullAllUsers', function(request, response){
 
-    console.log('pullAllUsers initiated from api.js...');
-
     var users = [];
 
     pg.connect(connectionString, function(error, client){
@@ -20,7 +18,7 @@ router.get('/pullAllUsers', function(request, response){
             console.log(error);
         }
 
-        var query = client.query("SELECT * FROM users");
+        var query = client.query("SELECT * FROM users ORDER BY id ASC");
 
         query.on('row', function(row){
             users.push(row);
@@ -29,6 +27,33 @@ router.get('/pullAllUsers', function(request, response){
         query.on('end', function(){
             client.end();
             return response.json(users);
+        });
+
+    });
+
+});
+
+router.get('/pullUserAddresses/:USERID', function(request, response){
+
+    //console.log('pullUserAddresses api call initiated...');
+
+    var addresses = [];
+
+    pg.connect(connectionString, function(error, client){
+
+        if(error){
+            console.log(error);
+        }
+
+        var query = client.query("SELECT users.name, addresses.* FROM users JOIN addresses ON users.id = addresses.user_id WHERE users.id = " + [request.params.USERID]);
+
+        query.on('row', function(row){
+            addresses.push(row);
+        });
+
+        query.on('end', function(){
+            client.end();
+            return response.json(addresses);
         });
 
     });
